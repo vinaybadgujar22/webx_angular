a85fcc584171a6fe1950da7199e3add64b801acf
vinaybadgujar22 <vinaybadgujar22@gmail.com>
2022-02-23 10:59:00 +0530

    vb component commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VBComponent } from './vb/vb.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-angular-project1';
-}
+  }
--- src/app/vb/vb.component.ts
+++ src/app/vb/vb.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-vb',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VBComponent implements OnInit {
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
